b46a669dbce318da289740cbd3fd1557c90a11a2
ahmedadegbami <ahmed.adegbami@gmail.com>
2022-03-16 21:15:07 +0100

    updated

--- index.js
+++ index.js
@@ -39,6 +39,8 @@ document.body.appendChild(unOrderedList)
 //     You should now see the list-items color and background-color change at the click of the button.
 // -->
 
+//     1) Create two inputs for picking up colors in the page, and a button with "Apply Colors" as text
+
 for (let i = 0; i < 2; i++) {
     let inputs = document.createElement("input")
     inputs.type = "color"
@@ -48,13 +50,15 @@ for (let i = 0; i < 2; i++) {
 let button = document.createElement("button")
     button.innerHTML = 'Apply Button'
 
+    //     2) Grab all <li> in the page from the DOM, and save them in a variable called "listItems"
+    
 let alllist = document.getElementsByTagName("li")
 
 
 
 // <!-- MANDATORY JS EXERCISES (continue from yesterday's file for this exercise)
-//     1) Create two inputs for picking up colors in the page, and a button with "Apply Colors" as text
-//     2) Grab all <li> in the page from the DOM, and save them in a variable called "listItems"
+//     
+
 //     3) Grab the inputs references via DOM manipulation and save them in variables
 //     4) Create a function and attach it to the "Apply Colors" button click event
 //     5) Cycle listItems inside the function. Verify that the function and the loop work by logging in the console 
